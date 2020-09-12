55ee7133fb967bfb43803c94db87633919c6c7aa
SinaSystem <sinasystem47@gmail.com>
2020-09-12 19:25:01 +0430

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Woolha.com Flutter Tutorial',
-      home: _FadeTransitionExample(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: FadeTransitionExampleState(),
     );
   }
 }
 
-class _FadeTransitionExample extends StatefulWidget {
-  _FadeTransitionExampleState createState() => _FadeTransitionExampleState();
+class FadeTransitionExampleState extends StatefulWidget {
+  @override
+  _FadeTransitionExampleStateState createState() =>
+      _FadeTransitionExampleStateState();
 }
 
-class _FadeTransitionExampleState extends State<_FadeTransitionExample>
+class _FadeTransitionExampleStateState extends State<FadeTransitionExampleState>
     with TickerProviderStateMixin {
-
   AnimationController _controller;
   Animation<double> _animation;
 
@@ -31,10 +38,10 @@ class _FadeTransitionExampleState extends State<_FadeTransitionExample>
         vsync: this,
         value: 0,
         lowerBound: 0,
-        upperBound: 1
-    );
+        upperBound: 1);
+
     _animation =
-        CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn);
+        CurvedAnimation(parent: _controller, curve: Curves.bounceInOut);
 
     _controller.forward();
   }
@@ -51,11 +58,13 @@ class _FadeTransitionExampleState extends State<_FadeTransitionExample>
         title: Text('Flutter Tutorial'),
       ),
       body: Center(
-        child: FadeTransition(
-          opacity: _animation,
+        child: ScaleTransition(
+          scale: _animation,
           child: Center(
             child: Text(
-                'flutter', style: TextStyle(color: Colors.teal, fontSize: 50)),
+              'Flutter',
+              style: TextStyle(color: Colors.teal, fontSize: 50),
+            ),
           ),
         ),
       ),
