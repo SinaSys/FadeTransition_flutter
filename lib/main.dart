1f24b8a405c703bc03f56a74ac187bca204a1205
SinaSystem <sinasystem47@gmail.com>
2020-09-12 19:21:55 +0430

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Woolha.com Flutter Tutorial',
+      home: _FadeTransitionExample(),
+    );
+  }
+}
+
+class _FadeTransitionExample extends StatefulWidget {
+  _FadeTransitionExampleState createState() => _FadeTransitionExampleState();
+}
+
+class _FadeTransitionExampleState extends State<_FadeTransitionExample>
+    with TickerProviderStateMixin {
+
+  AnimationController _controller;
+  Animation<double> _animation;
+
+  initState() {
+    super.initState();
+
+    _controller = AnimationController(
+        duration: const Duration(milliseconds: 5000),
+        vsync: this,
+        value: 0,
+        lowerBound: 0,
+        upperBound: 1
+    );
+    _animation =
+        CurvedAnimation(parent: _controller, curve: Curves.fastOutSlowIn);
+
+    _controller.forward();
+  }
+
+  @override
+  dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Tutorial'),
+      ),
+      body: Center(
+        child: FadeTransition(
+          opacity: _animation,
+          child: Center(
+            child: Text(
+                'flutter', style: TextStyle(color: Colors.teal, fontSize: 50)),
+          ),
+        ),
+      ),
+    );
+  }
+}
