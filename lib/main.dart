d8272991fd23bf88ba3e1699adebd243a69095c7
SinaSystem <sinasystem47@gmail.com>
2020-09-14 01:18:20 +0430

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,61 +7,93 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Tutorial',
-      home: _AnimatedDefaultTextStyle(),
+      home: _AnimatedList(),
     );
   }
 }
 
-class _AnimatedDefaultTextStyle extends StatefulWidget {
+class _AnimatedList extends StatefulWidget {
 
-  _AnimatedDefaultTextStyleState createState() =>
-      _AnimatedDefaultTextStyleState();
+  _AnimatedListState createState() =>
+      _AnimatedListState();
 }
 
-class _AnimatedDefaultTextStyleState
-    extends State<_AnimatedDefaultTextStyle> {
+class _AnimatedListState
+    extends State<_AnimatedList> {
+  final GlobalKey<AnimatedListState> listKey = GlobalKey<AnimatedListState>();
 
-  bool _first = true;
+  List<String> _items = [];
 
-  double _fontSize = 60;
-  Color _color = Colors.blue;
+  Widget _buildItem(BuildContext context, int index, animation) {
+    String item = _items[index];
+    return SlideTransition(
+      position: Tween<Offset>(
+        begin: const Offset(-1, 0),
+        end: Offset.zero,
+      ).animate(animation),
+      child: ListTile(
+        title: Text(item),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Animated Default TextStyle"),
+        title: Text("Animated List"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Container(
-              height: 120,
-              child: AnimatedDefaultTextStyle(
-                duration: const Duration(milliseconds: 300),
-                style: TextStyle(
-                  fontSize: _fontSize,
-                  color: _color,
-                  fontWeight: FontWeight.bold,
-                ),
-                child: Text('Flutter'),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              height: double.infinity,
+              child: AnimatedList(
+                key: listKey,
+                initialItemCount: _items.length,
+                itemBuilder: (context, index, animation) {
+                  return _buildItem(context, index, animation);
+                },
               ),
             ),
-            FlatButton(
-              onPressed: () {
-                setState(() {
-                  _fontSize = _first ? 90 : 60;
-                  _color = _first ? Colors.blue : Colors.red;
-                  _first = !_first;
-                });
-              },
-              child: Text(
-                "CLICK ME!",
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  setState(() {
+                    listKey.currentState.insertItem(0, duration: const Duration(milliseconds: 500));
+                    _items = []
+                      ..add(DateTime.now().toIso8601String())
+                      ..addAll(_items);
+                  });
+                },
+                child: Text(
+                  "Add item to first",
+                ),
               ),
-            )
-          ],
-        ),
+              FlatButton(
+                onPressed: () {
+                  if (_items.length <= 1) return;
+                  listKey.currentState.removeItem(
+                      0,
+                          (_, animation) => _buildItem(context, 0, animation),
+                      duration: const Duration(milliseconds: 500)
+                  );
+                  setState(() {
+                    _items.removeAt(0);
+                  });
+                },
+                child: Text(
+                  "Remove first item",
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
